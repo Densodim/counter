a585070cc1d840200c6a444c5819165245cd80f4
Densodim <25denso@gmail.com>
2024-08-11 23:16:35 +0300

    add example from useEffect

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,14 +1,43 @@
 import { Meta, StoryObj } from "@storybook/react/*";
 import { Clock } from "./Clock";
+import { useEffect, useState } from "react";
 
 const meta: Meta<typeof Clock> = {
-   component: Clock,
- };
-  
- export default meta;
- type Story = StoryObj<typeof Clock>;
+  title: "Rect/Clock",
+  component: Clock,
+  //   parameters: {
+  //     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+  //     layout: "centered",
+  //   },
+  tags: ["autodocs"],
+};
 
- export const BaseExample = ()=>{
-   return <Clock/>
- }
-  
+export default meta;
+type Story = StoryObj<typeof Clock>;
+
+export const BaseExample = () => {
+  return <Clock />;
+};
+
+export const KeysTracker = () => {
+  const [text, setText] = useState("");
+
+  console.log("Component with" + text);
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      console.log(e.key);
+      setText((PrevState) => PrevState + e.key);
+    };
+
+    window.addEventListener("keypress", handler);
+    
+    return () => {
+      window.removeEventListener("keypress", handler);
+    };
+  }, []);
+  return <>Typed text: {text}</>;
+};
+
+// export const KeysTrackerStory = {
+//   render: <KeysTracker />,
+// };
